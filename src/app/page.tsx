14275c8e62c6fb66a358fdc9a466a36df5919b7c
jzcurious <paranoid.hpc.io@gmail.com>
2026-07-06 14:22:01 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-pink-50">
+            <h1 className="text-4xl font-serif text-pink-900 mb-8">
+                Наша свадьба 💍
+            </h1>
+            <p className="text-lg text-pink-700">Окружение настроено!</p>
+        </main>
+    );
+}
